8732e71429265443191bbd219c9246eb4845028b
salma <salmaessam6666@yahoo.com>
2021-01-08 01:57:52 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,28 @@
+var id=document.querySelector("#PAR");
+function ChangeBorderColor(val){
+    id.style.borderColor=val;
+}
+function ChangeBorder(val){
+    id.style.borderStyle=val;
+}
+function ChangeDecorate(val){
+    id.style.textDecoration=val;
+}
+function ChangeTransform(val){
+    id.style.textTransform=val;
+}
+function ChangeIndent(val){
+    id.style.textIndent=val;
+}
+function ChangeLSpace(val){
+    id.style.letterSpacing=val;
+}
+function ChangeHeight(val){
+    id.style.lineHeight=val;
+}
+function ChangeAlign(val){
+    id.style.textAlign=val;
+}
+function ChangeFont(val){
+    id.style.fontFamily=val;
+}
